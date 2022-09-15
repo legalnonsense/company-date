9e09827cd0a0ad62f7c10c22720fac30096196df
Jeff Filipovits <jrfiliplovits@gmail.com>
2022-09-15 10:41:32 -0400

    Revert "delete comments"; fix timespans
    This reverts commit 0961219f23e6ab5594f9106bc76e4d99cc7ce66b.

--- company-date.el
+++ company-date.el
@@ -25,7 +25,6 @@ then company will not attempt to complete.")
 (defvar company-date-bound-func #'point-at-bol
   "Function to set the bound for `looking-back'.
 There is undoubtedly a better candidate than `point-at-bol'.")
-;; Maybe:
 (setq company-date-bound-func (lambda () (unless (< (- (point) 10) 1)
 					   (- (point) 10) 1)))
 
@@ -33,17 +32,14 @@ There is undoubtedly a better candidate than `point-at-bol'.")
   (cl-case command
     (interactive (company-begin-backend 'company-date))
     (prefix
+     ;; I don't know if I have to do this, but it can't hurt
      (save-match-data 
        (and (memq major-mode company-date-modes)
 	    (looking-back company-date-re (funcall company-date-bound-func))
 	    (let* ((match (match-string-no-properties 0))
 		   (processed-match (->> match
 					 (downcase)
-					 (s-chop-prefix "<")
-					 ;; convert any customized user input to a form that `org-read-date' allows
-					 ;; specifically here, allow "yes" "yesterday" to propose to fill the previous date
-					 ;; and "tom" or "tomorrow" fill the next. E.g. "<Tom at 5pm" will propose
-					 ;; <2021-06-08 Tue 17:00> which is tomorrow's date today. 
+					 (s-chop-prefix company-date-prefix)
 					 (replace-regexp-in-string "yes\\(?:terday\\)?\\.?[[:space:]]"
 								   (concat 
 								    (ts-day-of-week-name (ts-adjust 'day -1 (ts-now)))
@@ -51,15 +47,13 @@ There is undoubtedly a better candidate than `point-at-bol'.")
 					 (replace-regexp-in-string "tom\\.?[[:space:]]?"
 								   (concat 
 								    (ts-day-of-week-name (ts-adjust 'day 1 (ts-now)))
-								    " ")))))
-	      ;; I do not know how to avoid this without resorting to a global variable
-	      ;; (I tried returning match as a cons cell of the original and modified text,
-	      ;; but I think that company is hardwired for it to be a string.  
-	      (setq company-date-processed-result processed-match)	      
+								    " ")))))	      
+	      (setq company-date-processed-result processed-match)
 	      match))))
     (candidates
      ;; this inserts the match selected by the user. It automatically deletes the entered text,
      ;; and replaces it with the new text. 
+
      (with-temp-buffer
        (let*
 	   ;; HACK: I just wanted to get it done.  It's not pretty. 
@@ -68,58 +62,66 @@ There is undoubtedly a better candidate than `point-at-bol'.")
 		      company-date-processed-result))
 	    (date (if (listp result)
 		      (cl-loop for r in result
+			       when (not (string-match "[^[:digit:]APMapm]" (cadr result)))
+			       do (setf (cadr result) (concat (car (s-split " " (car result)))
+							      " " 
+							      (cadr result)))
 			       collect (org-read-date t t r))
 		    (org-read-date t t result))))
 	 (if (listp result)
-	     (let ((date1 (company-date--buffer-mod-to-string
-			   (org-insert-time-stamp (car date) nil)))
-		   (date2 (company-date--buffer-mod-to-string
-			   (org-insert-time-stamp (cadr date) nil))))
-	       (cond ((string= date1 date2)
-		      (org-insert-time-stamp (car date) t nil)
-		      (let ((hhmm (--> (company-date--buffer-mod-to-string
-					(org-insert-time-stamp (cadr date) t))
-				       (ts-parse-org it)
-				       (concat 
-					(ts-hour it)
-					":"
-					(ts-minute it)))))
-			(backward-char 1)
-			(insert "-")
-			(insert hhmm)
-			(end-of-line)
+	     (progn 
+	       (let ((date1 (company-date--buffer-mod-to-string
+			     (org-insert-time-stamp (car date) nil)))
+		     (date2 (company-date--buffer-mod-to-string
+			     (org-insert-time-stamp (cadr date) nil))))
+		 (cond ((string= date1 date2)
+			(org-insert-time-stamp (car date) t nil)
+			(let ((hhmm (--> (company-date--buffer-mod-to-string
+					  (org-insert-time-stamp (cadr date) t))
+					 (ts-parse-org it)
+					 (concat
+					  (s-pad-left 2 "0"
+						      (number-to-string (ts-hour it)))
+					  ":"
+					  (s-pad-left 2 "0"
+						      (number-to-string (ts-minute it)))))))
+			  (backward-char 1)
+			  (insert "-")
+			  (insert hhmm)
+			  (end-of-line)
+			  (insert "\n")
+			  (org-insert-time-stamp (car date) t t)
+			  (let ((hhmm (--> (company-date--buffer-mod-to-string
+					    (org-insert-time-stamp (cadr date) t))
+					   (ts-parse-org it)
+					   (concat
+					    (s-pad-left 2 "0"
+							(number-to-string (ts-hour it)))
+					    ":"
+					    (s-pad-left 2 "0"
+							(number-to-string (ts-minute it)))))))
+			    (backward-char 1)
+			    (insert "-")
+			    (insert hhmm)
+			    (end-of-line)
+			    (insert "\n"))))
+		       (t
+			(org-insert-time-stamp (car date) nil nil)
+			(insert "--")
+			(org-insert-time-stamp (cadr date) nil nil)
 			(insert "\n")
-			(org-insert-time-stamp date nil t)
-			(insert "\n"))
-		      (let ((hhmm (--> (company-date--buffer-mod-to-string
-					(org-insert-time-stamp (cadr date) t t))
-				       (ts-parse-org it)
-				       (concat 
-					(ts-hour it)
-					":"
-					(ts-minute it)))))
-			(backward-char 1)
-			(insert "-")
-			(insert hhmm)
-			(end-of-line)
-			(insert "\n")))
-		     (t 
-		      (org-insert-time-stamp (car date) nil nil)
-		      (insert "--")
-		      (org-insert-time-stamp (cadr date) nil nil)
-		      (insert "\n")
-		      (org-insert-time-stamp (car date) t nil)
-		      (insert "--")
-		      (org-insert-time-stamp (cadr date) t nil)
-		      (insert "\n")
-		      (org-insert-time-stamp (car date) nil t)
-		      (insert "--")
-		      (org-insert-time-stamp (cadr date) nil t)
-		      (insert "\n")
-		      (org-insert-time-stamp (car date) t t)
-		      (insert "--")
-		      (org-insert-time-stamp (cadr date) t t)
-		      (insert "\n"))))
+			(org-insert-time-stamp (car date) t nil)
+			(insert "--")
+			(org-insert-time-stamp (cadr date) t nil)
+			(insert "\n")
+			(org-insert-time-stamp (car date) nil t)
+			(insert "--")
+			(org-insert-time-stamp (cadr date) nil t)
+			(insert "\n")
+			(org-insert-time-stamp (car date) t t)
+			(insert "--")
+			(org-insert-time-stamp (cadr date) t t)
+			(insert "\n")))))
 	   (org-insert-time-stamp date nil nil)
 	   (insert "\n")			       
 	   (org-insert-time-stamp date t nil)
