288c11be856b2ae6342c5a9d4cc68c68c7f4a46a
Jeff Filipovits <jrfiliplovits@gmail.com>
2022-09-22 15:44:28 -0400

    add variable instead of fixed string

--- company-date.el
+++ company-date.el
@@ -90,7 +90,7 @@ There is undoubtedly a better candidate than `point-at-bol'.")
 	 (match-string 0))))
     (candidates
      (setq xxx arg)
-     (if (string= "<< " arg)
+     (if (string= (concat company-date-prefix " ") arg)
 	 company-date--past-dates
        (company-date--triage-input arg)))
     (post-completion
